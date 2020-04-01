c9c70e7bc5d9adc506f3d1e2e9125a3bc14b3ffc
liuliu <liuliu@hfepay.com>
2020-04-01 14:31:02 +0800

    提交run.sh自动拉起更新

--- runConf/run.sh
+++ runConf/run.sh
@@ -5,5 +5,17 @@ runPath=`pwd`
 export LD_LIBRARY_PATH=$runPath/lib:/usr/local/lib:$runPath/lib/freeswitch/mod:$LD_LIBRARY_PATH
 
 
-#$runPath/bin/freeswitch  -base $runPath -conf $runPath/etc/freeswitch -log $runPath/var/log -db $runPath/var/lib/freeswitch/db -mod $runPath/lib/freeswitch/mod
-$runPath/bin/freeswitch -nonat -base $runPath -conf $runPath/etc/freeswitch -log /data/t3-logs/freeswitch/ -db $runPath/var/lib/freeswitch/db -mod $runPath/lib/freeswitch/mod -recordings $runPath/recordings 
+#$runPath/bin/freeswitch -nonat -base $runPath -conf $runPath/etc/freeswitch -log /data/t3-logs/freeswitch/ -db $runPath/var/lib/freeswitch/db -mod $runPath/lib/freeswitch/mod -recordings $runPath/recordings 
+
+while [ 1 ]
+do
+        count=`ps -ef | grep "freeswitch" |grep -v grep |wc -l`
+        if [ 0 == $count ]
+        then
+                echo "start process....."
+                $runPath/bin/freeswitch -nonat -base $runPath -conf $runPath/etc/freeswitch -log /data/t3-logs/freeswitch/ -db $runPath/var/lib/freeswitch/db -mod $runPath/lib/freeswitch/mod -recordings $runPath/recordings 
+        else
+                echo "runing....."
+        fi
+        sleep 5
+done
